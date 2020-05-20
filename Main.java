5465e345cc00252b559ee13f1560c7652c818764
matuszm92 <marceli.matuszak@gmail.com>
2020-05-20 15:17:46 +0000

    Add Person class

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello home!");
     System.out.println("Hello home rly!");
+    Person person1 = new Person();
+    person1.firstName = "Ala";
+    System.out.println(person1.firstName);
   }
 }
